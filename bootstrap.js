f2b78cd6e261413788f7cf04b1060e45d62d7c04
Matthias Hryniszak <padcom@gmail.com>
2023-10-31 03:04:38 +0100

    Code cleanup

--- bootstrap.js
+++ bootstrap.js
@@ -16,40 +16,32 @@ function map(property, from, on) {
   })
 }
 
-/**
- * Main application entry point
- */
-function main() {
-  // Start a new P5 Sketch!
-  new P5(p5 => {
-    // make functions context-bound to p5
-    for (const field in p5)
-      if (typeof p5[field] === 'function')
-        p5[field] = p5[field].bind(p5)
+// Start a new P5 Sketch!
+new P5(p5 => {
+  // make functions context-bound to p5
+  for (const field in p5)
+    if (typeof p5[field] === 'function')
+      p5[field] = p5[field].bind(p5)
 
-    // make p5 functions publicly available and properties publicly accessible
-    for (const field in p5)
-      switch (typeof p5[field]) {
+  // make p5 functions publicly available and properties publicly accessible
+  for (const field in p5)
+    switch (typeof p5[field]) {
+      case 'function':
+        window[field] = p5[field]
+        break
+      default:
+        map(field, p5, window)
+        break
+    }
+
+  // assign exported elements to the p5 instance
+  for (const field in sketch)
+    if ([typeof sketch[field], 'undefined'].includes(typeof p5[field]))
+      switch(typeof sketch[field]) {
         case 'function':
-          window[field] = p5[field]
+          p5[field] = () => sketch[field](p5)
           break
         default:
-          map(field, p5, window)
-          break
+          p5[field] = sketch[field]
       }
-
-    // assign exported elements to the p5 instance
-    for (const field in sketch)
-      if ([typeof sketch[field], 'undefined'].includes(typeof p5[field]))
-        switch(typeof sketch[field]) {
-          case 'function':
-            p5[field] = () => sketch[field](p5)
-            break
-          default:
-            p5[field] = sketch[field]
-        }
-  })
-}
-
-// Start the main function as soon as the document is loaded
-window.addEventListener('load', main, { once: true })
+})
